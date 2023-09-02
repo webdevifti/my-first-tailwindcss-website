2b7254ed7a1f6a3ea8e4d34b93c38757e616a54d
Eftekhar Alam <eftialam@hotmail.com>
2023-09-02 17:42:15 +0600

    state added

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode:'class',
-  content: ["./index.html","./page.html","./content.html","./login.html"],
+  content: ["./index.html","./login.html"],
   theme: {
     extend:{
       fontFamily: {
