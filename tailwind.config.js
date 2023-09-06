d7278c84707f72f1cd1f6aae36eabad78069d854
Eftekhar Alam <eftialam@hotmail.com>
2023-09-06 15:04:33 +0600

    chat window design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode:'class',
-  content: ["./index.html","./login.html","./contact.html"],
+  content: ["./index.html","./login.html","./contact.html","./chat.html"],
   theme: {
     extend:{
       fontFamily: {
